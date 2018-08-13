2e116cb67d8bef2143b1e6f6bd4097e16ffb5f76
Jury Soldatenkov <yurysold@gmail.com>
2018-08-13 21:33:23 +0300

    Created async network stream

---
+++ src/SolarWind/Internals/AsyncNetworkStream.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Codestellation.SolarWind.Threading;
+
+namespace Codestellation.SolarWind.Internals
+{
+    public class AsyncNetworkStream : NetworkStream
+    {
+        private readonly SocketAsyncEventArgs _receiveArgs;
+        private readonly AutoResetValueTaskSource<int> _receiveSource;
+        private readonly SocketAsyncEventArgs _sendArgs;
+        private readonly AutoResetValueTaskSource<int> _sendSource;
+
+        public AsyncNetworkStream(Socket socket) : base(socket, true)
+        {
+            _receiveArgs = new SocketAsyncEventArgs();
+            _receiveArgs.Completed += OnReceiveCompleted;
+            _receiveSource = new AutoResetValueTaskSource<int>();
+
+            _sendArgs = new SocketAsyncEventArgs();
+            _sendArgs.Completed += OnSendCompleted;
+            _sendSource = new AutoResetValueTaskSource<int>();
+        }
+
+        private void OnSendCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success)
+            {
+                _sendSource.SetResult(e.BytesTransferred);
+            }
+            else
+            {
+                _sendSource.SetException(new SocketException((int)e.SocketError));
+            }
+        }
+
+        private void OnReceiveCompleted(object sender, SocketAsyncEventArgs e)
+        {
+            if (e.SocketError == SocketError.Success)
+            {
+                _receiveSource.SetResult(e.BytesTransferred);
+            }
+            else
+            {
+                _receiveSource.SetException(new SocketException((int)e.SocketError));
+            }
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            Console.WriteLine();
+            //TODO: Check out how this implemented and whether I have to override it
+
+            return base.FlushAsync(cancellationToken);
+        }
+
+        //TODO: I can do it with value task but I have to clone ReadAsync method on the SslStream and use both of them. 
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellation)
+        {
+            if (Socket.Available != 0)
+            {
+                var received = Socket.Receive(buffer, offset, count, SocketFlags.None);
+                return Task.FromResult(received);
+            }
+
+            _receiveArgs.SetBuffer(buffer, offset, count);
+            if (!Socket.ReceiveAsync(_receiveArgs))
+            {
+                throw new InvalidOperationException("Handle it gracefully");
+            }
+
+            return _receiveSource.AwaitValue(cancellation).AsTask();
+        }
+
+        public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            var left = count;
+            var sent = 0;
+            while (left != 0)
+            {
+                var realOffset = offset + sent;
+
+                _sendArgs.SetBuffer(buffer, realOffset, left);
+
+                if (!Socket.SendAsync(_sendArgs))
+                {
+                    throw new InvalidOperationException("Handle it gracefully");
+                }
+
+                sent += await _sendSource
+                    .AwaitValue(cancellationToken)
+                    .ConfigureAwait(false);
+                left -= sent;
+            }
+        }
+    }
+}
